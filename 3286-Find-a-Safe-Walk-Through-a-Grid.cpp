371a89b025a734b38a74104f21b72aa13648f81a
Sowmik Sarker <36860940+Sowmik23@users.noreply.github.com>
2026-07-02 21:42:52 +0600

    [3286] [Time Beats: 92.60%] [Memory Beats: 47.15%] - LeetPush

--- 3286-Find-a-Safe-Walk-Through-a-Grid.cpp
+++ 3286-Find-a-Safe-Walk-Through-a-Grid.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         
+        /** DIJKSTRA
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dis(m, vector<int> (n, -1));
@@ -24,5 +25,47 @@ public:
             }
         }
         return dis[m-1][n-1] < health;
+        */
+
+        //BFS
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dis(m, vector<int>(n, INT_MAX));
+        int dirs[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        deque<pair<int, int>> q;
+        
+        q.emplace_front(0, 0);
+        dis[0][0] = grid[0][0];
+
+        while (!q.empty()) {
+            auto [cx, cy] = q.front();
+            q.pop_front();
+            // the first time it leaves the queue, the shortest distance is
+            // guaranteed
+            if (cx == m - 1 && cy == n - 1) {
+                return true;
+            }
+
+            for (auto& [dx, dy] : dirs) {
+                int nx = cx + dx, ny = cy + dy;
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
+                    continue;
+                }
+                int cost = dis[cx][cy] + grid[nx][ny];
+                // pruning: the new distance does not meet health requirements
+                if (cost >= health) {
+                    continue;
+                }
+                if (cost < dis[nx][ny]) {
+                    dis[nx][ny] = cost;
+                    if (grid[nx][ny] == 0) {
+                        q.emplace_front(nx, ny);
+                    } else {
+                        q.emplace_back(nx, ny);
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 };
