0a8d42bbd068107e138cfca3a17514cec98f2cfc
Sowmik Sarker <36860940+Sowmik23@users.noreply.github.com>
2026-09-07 22:28:16 +0600

    [940] [Time Beats: 100.00%] [Memory Beats: 55.30%] - LeetPush

---
+++ 940-Distinct-Subsequences-II.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        
+        /*
+        int n = s.size();
+        int MOD = 1e9 + 7;
+
+        vector<int> dp(n, 1);
+        int result = 0;
+
+        for(int i = 0; i < n; i++){
+            // Try extending subsequences ending before i
+            for(int j = 0; j < i; j++){
+                if(s[i] != s[j]) {
+                    dp[i] = (dp[i] + dp[j]) % MOD;
+                }
+            }
+            result = (result + dp[i]) % MOD;
+        }
+        return result;
+        */
+
+        //optimization
+        const int N = s.length();
+        const int MOD = 1e9 + 7;
+        
+        vector<int> dp(N+1);
+        dp[0] = 1;
+        vector<int> last(26, -1);
+        
+        for(int i = 0; i < N; i++){
+            int x = s[i] - 'a';
+            dp[i+1] = dp[i] * 2 % MOD;
+            if(last[x] >= 0) // if this is the first occurence of ch
+                dp[i+1] -= dp[last[x]];
+            dp[i+1] %= MOD;
+            last[x] = i;
+        }
+        dp[N]--;
+        if(dp[N] < 0) dp[N] += MOD;
+        return dp[N];
+    }
+};
